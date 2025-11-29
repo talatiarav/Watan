9be65417eebfe30b413f44943d6e8d246f8322c5
Lakshya Langalia <lakshya.langalia@gmail.com>
2025-11-28 21:34:48 -0500

    Update assessment.h

--- Watan-main/assessment.h
+++ Watan-main/assessment.h
@@ -1,6 +1,9 @@
 export module Assessment;
 
-// enum class that lists the different types of assessments
+// Represents the type of completion on a course criterion (or none).
+// Used to track whether a criterion is an Assignment, Midterm, Exam,
+// and to determine upgrade order and resource payouts.
+
 export enum class Assessment {
     Achievement,
     Assignment,
