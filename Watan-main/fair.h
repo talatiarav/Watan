ef47fe955f21337517988cb4f6d1e2c570ad1a5d
Lakshya Langalia <lakshya.langalia@gmail.com>
2025-11-28 20:29:31 -0500

    Update fair.h

--- Watan-main/fair.h
+++ Watan-main/fair.h
@@ -1,16 +1,13 @@
-#ifndef _FAIR_H_
-#define _FAIR_H_
+export module Fair;
 
-#include "dice.h"
+import Dice;
 
-class Fair: public Dice {
-	int dice1;
-	int dice2;
-	public:
-	Fair(); //ctor
-	void setDie(int num);
-	int roll();
+export class Fair : public Dice {
+    int dice1;
+    int dice2;
+
+public:
+    Fair(); // ctor
+    void setDie(int num) override;
+    int roll() override;
 };
-
-
-#endif
