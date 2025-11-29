1a5764d3446a38b125e0bf3457af56379bc86cbe
Lakshya Langalia <lakshya.langalia@gmail.com>
2025-11-28 21:47:29 -0500

    Update colour.cc

--- Watan-main/colour.cc
+++ Watan-main/colour.cc
@@ -3,6 +3,11 @@ module Colour;
 import <iostream>;
 import <cstdlib>;
 
+// Defines the Colour enum and how colours are printed.
+// Supports optional ANSI colour output for the board, but player names
+// always print in plain text. Also provides an operator<< for easy display.
+
+
 // Global flag for colored output (only affects board display in boardview.cc)
 static bool useColors = false;
 
@@ -11,7 +16,6 @@ void Colour_enableColors(bool enable) {
 }
 
 std::ostream &operator<<(std::ostream &out, Colour colour) {
-    // Always use plain text for player names (colors only on board)
     switch (colour) {
         case Colour::Blue:   return out << "Blue";
         case Colour::Red:    return out << "Red";
