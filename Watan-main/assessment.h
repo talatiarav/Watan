31a6d16a59be9633a6278e33d49013f4c6886d4c
Lakshya Langalia <lakshya.langalia@gmail.com>
2025-11-28 19:49:42 -0500

    Update assessment.h

--- Watan-main/assessment.h
+++ Watan-main/assessment.h
@@ -1,16 +1,13 @@
-#ifndef ASSESSMENT_H
-#define ASSESSMENT_H
+export module Assessment;
 
 // Single source of truth for “what’s built” at a location.
 // - On Vertex: Assignment / Midterm / Exam
 // - On Edge: Achievement
 // - Or None (unbuilt)
-enum class Assessment {
+export enum class Assessment {
     Achievement,
     Assignment,
     Midterm,
     Exam,
     None
 };
-
-#endif
